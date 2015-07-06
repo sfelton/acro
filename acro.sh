062a088f7850f75d0afe6c48d13ee73e8cd2a5ae
Sam Felton <samfelt@samfelt-m-p01h.ddns.asig.cisco.com>
2015-07-06 10:02:59 -0400

    first commit

---
+++ acro.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# The point of this is to look up an acronym from an acronym file and display
+# what it is
+
+
+ACRO_FILE="acronyms.txt"
+MAX_LINES=$(tput lines)
+
+#Functions
+function usage()
+{
+    echo Provide acronym to look up
+    echo    ex. acro TLA
+    echo 
+    echo Add acronym with \'acro add\'
+    echo List all acronyms with \'acro all\'
+}
+
+function add_acronym()
+{
+    read -p "What is the ACRONYM? " ACRO
+    read -p "What is the MEANING? " MEANING
+    cp -f $ACRO_FILE $ACRO_FILEi.backup
+    printf "%-8s| " $ACRO >> $ACRO_FILE
+    echo $MEANING >> $ACRO_FILE
+    sort $ACRO_FILE --output=$ACRO_FILE
+}
+
+function find_acronym()
+{
+    count=$(fgrep --ignore-case --count $1 $ACRO_FILE)
+    
+    #-i == --ignore-case
+    if [ $count -gt $MAX_LINES ]; then
+        fgrep -i $1 $ACRO_FILE | less
+    else
+        fgrep -i $1 $ACRO_FILE
+    fi
+}
+
+#Make sure that the acro file actually exsists
+if [ ! -f "$ACRO_FILE" ]; then
+    echo $ACRO_FILE does not exsist
+    exit
+fi
+
+case $1 in
+    all | All)
+        less $ACRO_FILE
+        ;;
+    add | Add)
+        add_acronym
+        ;;
+    *)
+        find_acronym $1
+        ;;
+esac
+
